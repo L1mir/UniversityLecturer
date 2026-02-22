1c674d0abaf6565a8eddd79a171d2f066e1fece8
Limir <vladimir.puchynski@gmail.com>
2026-02-22 22:40:18 +0300

    first commit

---
+++ src/main/java/org/limir/universitylecture/UniversityLectureApplication.java
@@ -0,0 +1,13 @@
+package org.limir.universitylecture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniversityLectureApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UniversityLectureApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/limir/universitylecture/UniversityLectureApplicationTests.java
@@ -0,0 +1,13 @@
+package org.limir.universitylecture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniversityLectureApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
